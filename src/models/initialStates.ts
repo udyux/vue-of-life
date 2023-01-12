227abeb65bfe66ad6b47b13b6f12e7455b7587d1
Nicolas Udy <dev@udy.io>
2023-01-12 13:42:45 -0500

    feat: add War initial state

--- src/models/initialStates.ts
+++ src/models/initialStates.ts
@@ -2,6 +2,7 @@ import type { Coordinates } from '@/types/grid';
 import anchorSymmetry from '@/assets/data/grid-states/Anchor-Symmetry.json';
 import cmcFloat from '@/assets/data/grid-states/CMC-Float.json';
 import totemOscillator from '@/assets/data/grid-states/Totem-Oscillator.json';
+import war from '@/assets/data/grid-states/War.json';
 
 export default {
   anchorSymmetry: {
@@ -15,5 +16,9 @@ export default {
   totemOscillator: {
     label: 'Totem-Oscillator',
     state: totemOscillator as Coordinates[],
+  },
+  war: {
+    label: 'War',
+    state: war as Coordinates[],
   },
 };
